86648f2dd2883ac1ee7116312c9f9f8d8e013e28
boo <boo@boo.localhost>
2021-01-17 16:14:32 +0100

    Menu, tags, links

--- app/Core/Util/Tags.php
+++ app/Core/Util/Tags.php
@@ -38,4 +38,38 @@ class Tags
 		$doc->loadHTML(mb_convert_encoding($html, 'HTML-ENTITIES', 'UTF-8'), LIBXML_HTML_NOIMPLIED | LIBXML_HTML_NODEFDTD);
 		return $doc->saveHTML();
 	}
+
+	/**
+	 * Replace to html tags
+	 *
+	 * @param string $str Text with [h1], ... tags
+	 * @return string Html content
+	 */
+	static function Replace($str) {
+		$tags = [];
+		$str = strip_tags($str);
+		$str = preg_replace_callback('~(\[\/?[a-z]+[0-9]?+\]?|\[>\])~i',
+		function ($match) use (&$tags)
+		{
+			$m = '';
+			array_push($tags, $match[1]);
+			if($match[1] === '[>]') {
+				return '>';
+			} else {
+				$m = str_replace('[','<', $match[1]);
+				return str_replace(']','>', $m);
+			}
+		},
+		$str);
+		return $str;
+	}
+
+	static function ReplaceSample() {
+		return '
+		Tag [h1]Hello text[/h1]
+		Link [a href="https://do.do/url" [>] Hello link[/a]
+		Hello imag [img src="https://do.do/imahe.png" [>]
+		Div [div class="" [>] Hello text[/div]
+		';
+	}
 }
